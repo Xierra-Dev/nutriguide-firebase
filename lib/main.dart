5cfc0ee05688ec29c2c65e4ecce6f34d9fb91cb1
Simon1705 <simonpeter@student.telkomuniversity.ac.id>
2024-12-06 19:42:28 +0700

    exit app, gaperlu login ulang

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'firebase_options.dart';
 import 'landing_page.dart';
-
+import 'home_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -23,7 +24,20 @@ class MealPlannerApp extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
         fontFamily: 'Roboto',
       ),
-      home: const LandingPage(),
+      home: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          
+          if (snapshot.hasData && snapshot.data != null) {
+            return const HomePage();
+          }
+          
+          return const LandingPage();
+        },
+      ),
     );
   }
 }
